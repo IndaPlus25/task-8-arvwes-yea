1e0127e5938a17118ab8b646c2dc9a2df0fbfccb
lenarv <lenarv@ArvidsLenovo.localdomain>
2025-11-06 23:39:03 +0100

    commented assembler

--- rust-assembler/assembler/src/assembler.rs
+++ rust-assembler/assembler/src/assembler.rs
@@ -14,7 +14,7 @@ const R0: Registry = 0b00;
 const R1: Registry = 0b10;
 const R2: Registry = 0b01;
 const R3: Registry = 0b11;
-
+//board
 const BOARD: [[&str; 7]; 7] = [
     ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
     ["S ", "  ", "1 ", "  ", "SU", "  ", "J "],
@@ -24,43 +24,51 @@ const BOARD: [[&str; 7]; 7] = [
     ["  ", "  ", "  ", "  ", "R2", "  ", "  "],
     ["R0", "  ", "R1", "  ", "  ", "R3", "  "],
 ];
-
+/**
+ * Converts an input of directions and stars (<>^v and *) to functions located on the board
+ * then converts the functions to binary
+ * returns the binary code for all functions as a vector of binary strings
+ */
 pub fn assemble(input: &str) -> Vec<u8> {
-    let mut man: (usize, usize) = (0, 0);
-    let paths: std::str::Split<'_, char> = input.split('*');
-    let mut result: Vec<u8> = Vec::new();
+    let mut player: (usize, usize) = (0, 0); // sets player starting position to top left
+
+    let paths: std::str::Split<'_, char> = input.split('*');// splits the directions in to paths each path represents a command
+    let mut result: Vec<u8> = Vec::new(); //result vector
+
+    // loops trough all paths creating all commands and adding them to the result vector
     for path in paths {
-        let directions: std::str::Chars<'_> = path.chars();
-        let mut command: Option<u8> = None;
+        let directions: std::str::Chars<'_> = path.chars(); //creates an iterator of directions
+        let mut command: Option<u8> = None; // creates an empty command
         let mut uses_imm: Option<bool> = None;
+        // loops directions and determens mans position and converts position to operations
         for direction in directions {
             match direction {
                 '>' => {
-                    if man.0 == 6 {
-                        man.0 = 0;
+                    if player.0 == 6 {
+                        player.0 = 0; // if player walks off player appers on the opposite side
                     } else {
-                        man.0 += 1;
+                        player.0 += 1;
                     }
                 }
                 '<' => {
-                    if man.0 == 0 {
-                        man.0 = 6;
+                    if player.0 == 0 {
+                        player.0 = 6;// if player walks off player appers on the opposite side
                     } else {
-                        man.0 -= 1;
+                        player.0 -= 1;
                     }
                 }
                 'v' => {
-                    if man.1 == 6 {
-                        man.1 = 0;
+                    if player.1 == 6 {
+                        player.1 = 0;// if player walks off player appers on the opposite side
                     } else {
-                        man.1 += 1;
+                        player.1 += 1;
                     }
                 }
                 '^' => {
-                    if man.1 == 0 {
-                        man.1 = 6;
+                    if player.1 == 0 {
+                        player.1 = 6;// if player walks off player appers on the opposite side
                     } else {
-                        man.1 -= 1;
+                        player.1 -= 1;
                     }
                 }
                 _ => {}
@@ -68,8 +76,8 @@ pub fn assemble(input: &str) -> Vec<u8> {
 
             let mut segment: Option<u8> = None;
             let mut seg_len: u8 = 0;
-            let position: &str = BOARD[man.1][man.0];
-
+            let position: &str = BOARD[player.1][player.0];
+            // determan what operation the player is standing on and saves vaalue of operation as binary
             match position {
                 "J " => {
                     segment = Some(J);
@@ -188,14 +196,19 @@ pub fn assemble(input: &str) -> Vec<u8> {
                 }
                 _ => {}
             }
+            // creates a command if command does not exist and if it does exist the next operation segment is added
             if command.is_none() && segment.is_some() {
                 command = segment;
             } else if segment.is_some() && command.unwrap() != J &&command.unwrap() != JB{
-                command = Some((command.unwrap() << seg_len) | (segment.unwrap()));
+                // shifts the command to the left to make room for the segment and adds the segment (if it isn't a jump command)
+                command = Some((command.unwrap() << seg_len) | (segment.unwrap()));  
             }else if segment.is_some() {
+                // if its a jump command it is allways shifted 5blocks since a jump comand is 3bits for operation and 5 for jump distance
+                // doing it like this is not recomended
                  command = Some((command.unwrap() << 5) | (segment.unwrap()));
             }
         }
+        // if its an operation which wants a flag a flag is added
         if command.is_some() {
             if uses_imm.is_some() {
                 println!("will add flag");
@@ -205,9 +218,9 @@ pub fn assemble(input: &str) -> Vec<u8> {
                     command = Some((command.unwrap() << 1) | 0);
                 }
             }
-
+            // pushes 8 bit command to result vector
             result.push(command.unwrap());
-            command = Some(0);
+
         }
     }
     return result;
