52ebb68baa83d000398c7316d3c12523f9ed60e7
lenarv <lenarv@ArvidsLenovo.localdomain>
2025-11-07 11:30:32 +0100

    added to factorial almost done

--- OnBoard/rust-assembler/assembler/target/debug/assembler.d
+++ OnBoard/rust-assembler/assembler/target/debug/assembler.d
@@ -1,1 +1,1 @@
-/home/lenarv/task-8-arvwes-yea/rust-assembler/assembler/target/debug/assembler: /home/lenarv/task-8-arvwes-yea/rust-assembler/assembler/src/assembler.rs /home/lenarv/task-8-arvwes-yea/rust-assembler/assembler/src/main.rs
+/home/lenarv/task-8-arvwes-yea/OnBoard/rust-assembler/assembler/target/debug/assembler: /home/lenarv/task-8-arvwes-yea/OnBoard/rust-assembler/assembler/src/assembler.rs /home/lenarv/task-8-arvwes-yea/OnBoard/rust-assembler/assembler/src/main.rs
